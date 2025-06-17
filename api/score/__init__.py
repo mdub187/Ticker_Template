2c990eceaea636e8d61a03225ff4ff6ffd9acc4f
mdub187 <maweeks.91@gmail.com>
2025-06-17 02:06:26 -0600

    2

---
+++ api/score/__init__.py
@@ -0,0 +1,6 @@
+import requests
+import response
+from requests import headers
+
+if __name__ == '__main__':
+    print(requests, response)
---
+++ client/__init__.py
@@ -0,0 +1,6 @@
+# Expose the gui module
+from client import *
+import gui
+import login
+if __name__ == '__main__':
+    print(__name__)
